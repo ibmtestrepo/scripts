caaf79de327a515c32e2d9eb215c058ad9314548
ibmtestrepo <37136332+ibmtestrepo@users.noreply.github.com>
2018-03-07 14:31:10 +0530

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export ${EnvironmentVariable}
+echo export ${EnvironmentVariable} >> /home/ec2-user/.bashrc
+echo ${FileContents} >> /etc/abc.txt\n
+yum install -y ${InstallPackages}
+echo `date` >> /etc/data_bala.txt
